1386d33028a297005d47716727978ceae028f1cc
Kadika <dryan10101@gmail.com>
2022-12-07 15:38:59 -0800

    initials.cpp program added; created learned-cpps folder for holding older files

---
+++ initials.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+    std::cout << "DDDD   RRRR\n";
+    std::cout << "D   D  R   R\n";
+    std::cout << "D   D  R   R\n";
+    std::cout << "D   D  RRRR\n";
+    std::cout << "D   D  R   R\n";
+    std::cout << "D   D  R    R\n";
+    std::cout << "DDDD   R     R\n";
+    return 0;
+}
