4ba475a767b46f9a5b107cd1a7065f0d49271440
Kadika <dryan10101@gmail.com>
2022-12-11 20:32:14 -0800

    leap year evaluator, logical operators practice

---
+++ logic.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+  
+  int year;
+
+  std::cout << "Enter year: ";
+  std::cin >> year;
+
+  if ((year > 999) && (year % 4 == 0) && (year % 100 == 0) && (year % 400 == 0)) {
+    
+  }
+  
+}
