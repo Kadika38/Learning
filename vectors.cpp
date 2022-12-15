7dc60b1b735a81a5e0f2d98a38762803c8d77bee
Kadika <dryan10101@gmail.com>
2022-12-14 16:27:41 -0800

    whale speak translator without debugging couts

--- vectors.cpp
+++ vectors.cpp
@@ -11,18 +11,14 @@ int main() {
   std::vector<char> result;
 
   for (int i = 0; i < original.size(); i++) {
-    std::cout << "O: " << original[i] << " -> ";
     for (int j = 0; j < vowels.size(); j++) {
       if (original[i] == vowels[j]) {
         result.push_back(vowels[j]);
-        std::cout << vowels[j];
         if (vowels[j] == 'e' || vowels[j] == 'u') {
           result.push_back(vowels[j]);
-          std::cout << vowels[j];
         }
       }
     }
-    std::cout << "\n";
   }
 
   for (int f = 0; f < result.size(); f++) {
