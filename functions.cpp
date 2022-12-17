39baf558e676f21718806972ea8ffc7c00b811c8
Kadika <dryan10101@gmail.com>
2022-12-16 16:11:16 -0800

    more function practice

--- functions.cpp
+++ functions.cpp
@@ -1,19 +1,19 @@
 #include <iostream>
+#include <cmath>
 
-// Define name_x_times() below:
-void name_x_times(std::string name, int x) {
-  while (x > 0) {
-    std::cout << name;
-    x--;
+// Define tenth_power() here:
+int tenth_power(int num) {
+  int hold = num;
+  for (int i = 9; i > 0; i--) {
+    hold = hold * num;
   }
+  return hold;
 }
-
 
 int main() {
   
-  std::string my_name = "David";
-  int some_number = 5; // Change this if you like!
-  // Call name_x_times() below with my_name and some_number
-  name_x_times(my_name, some_number);
+  std::cout << tenth_power(0) << "\n";
+  std::cout << tenth_power(1) << "\n";
+  std::cout << tenth_power(2) << "\n";
   
 }
