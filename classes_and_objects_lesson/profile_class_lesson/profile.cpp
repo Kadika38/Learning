a66c0af898146713e13f22f81fc6f0c1ad95d56f
Kadika <dryan10101@gmail.com>
2023-01-05 17:19:17 -0800

    continued work on profile class lesson

--- classes_and_objects_lesson/profile_class_lesson/profile.cpp
+++ classes_and_objects_lesson/profile_class_lesson/profile.cpp
@@ -15,6 +15,23 @@ std::string Profile::view_profile() {
   profile_summary +=  "\nPronouns: " + pronouns;
   profile_summary += "\nLocation: " + city;
   profile_summary += ", " + country;
-  profile_summary += "\n";
+  if (hobbies.size() > 0) {
+    profile_summary += "\nHobbies: ";
+    for (int i = 0; i < hobbies.size(); i++) {
+      profile_summary += hobbies[i];
+      if (i < hobbies.size() - 2) {
+        profile_summary += ", ";
+      } else if (i == hobbies.size() - 2) {
+        profile_summary += ", and ";
+      } else {
+        profile_summary += "\n";
+      }
+    }
+  }
+
   return profile_summary;
+}
+
+void Profile::add_hobby(std::string new_hobby) {
+  hobbies.push_back(new_hobby);
 }
--- classes_and_objects_lesson/profile_class_lesson/profile.hpp
+++ classes_and_objects_lesson/profile_class_lesson/profile.hpp
@@ -17,5 +17,6 @@ class Profile {
   Profile(std::string new_name, int new_age, std::string new_city, std::string new_country, std::string new_pronouns = "they/them");
 
   std::string view_profile();
+  void add_hobby(std::string new_hobby);
 
 };
