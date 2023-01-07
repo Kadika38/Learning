5a9669fecba68e98c183311d126f039e4f20f75d
Kadika <dryan10101@gmail.com>
2023-01-06 16:37:48 -0800

    finished bleep pass-by-reference algo problem

--- references_and_pointers_lesson/bleep_project/functions.cpp
+++ references_and_pointers_lesson/bleep_project/functions.cpp
@@ -1,12 +1,20 @@
 #include "functions.hpp"
 
 void bleep(std::string const &bad_word, std::string &literature) {
-  int bw_sz = bad_word.size();
-  int counter = 0;
+  int bwsz = bad_word.size();
 
+  int counter = 0;
   for (int i = 0; i < literature.size(); i++) {
-    if (counter == 0 && literature[i] == bad_word[0]) {
-      //to be continued
+    if (literature[i] == bad_word[counter]) {
+      counter++;
+    } else if (counter > 0) {
+      counter = 0;
+    }
+    if (counter == bwsz) {
+      for (int j = i; j > i - bwsz; j--) {
+        literature[j] = '*';
+      }
+      counter = 0;
     }
   }
 }
