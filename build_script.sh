9429e9e670f591b23a2146613e42419f8eb409f2
Kadika <dryan10101@gmail.com>
2023-01-12 18:28:10 -0800

    finished build script bash scripting project

--- build_script.sh
+++ build_script.sh
@@ -7,11 +7,28 @@ echo $version
 echo "Continue? (1 == yes | 0 == no)"
 read versioncontinue
 if [ versioncontinue == 1 ]
-do
+sourcefiles=./source/*
+then
   echo "OK"
-  for file in ./source
+  for file in $sourcefiles
   do
-  echo file
+    echo $file
+    if [ "$file" == "./source/secretinfo.md" ]
+    then
+      echo "The above file will not be copied into the Build"
+    else
+      echo "The above file is being copied into the Build"
+      cp $file ./build
+    fi
+  done
+  echo "Using version $version, the following files have been copied into the Build"
+  cd build
+  builddir=./*
+  for file in $builddir
+  do
+    echo $file
+  done
+  cd ..
 else
   echo "Please come back when you are ready"
 fi
