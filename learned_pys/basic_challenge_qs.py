3ea4134d8c908c79e6b72eacc9b3796ff93987fb
David <dryan10101@gmail.com>
2023-01-20 01:58:55 -0800

    basic python list challenges

--- learned_pys/basic_challenge_qs.py
+++ learned_pys/basic_challenge_qs.py
@@ -1,3 +1,5 @@
+#basic challenges
+
 def large_power(base, exponent):
   if base ** exponent > 5000:
     return True
@@ -56,3 +58,35 @@ def max_num(num1, num2, num3):
   if (biggest == num1 and biggest == num2) or (biggest == num1 and biggest == num3) or (biggest == num2 and biggest == num3):
     return "It's a tie!"
   return biggest
+
+# list challenges
+
+def append_size(lst):
+  lst.append(len(lst))
+  return lst
+
+def append_sum(lst):
+  lst.append(lst[-1] + lst[-2])
+  lst.append(lst[-1] + lst[-2])
+  lst.append(lst[-1] + lst[-2])
+  return lst
+
+def larger_list(lst1, lst2):
+  if len(lst2) > len(lst1):
+    return lst2[-1]
+  return lst1[-1]
+
+def more_than_n(lst, item, n):
+  count = 0
+  for i in lst:
+    if i == item:
+      count += 1
+  if count > n:
+    return True
+  return False
+
+def combine_sort(lst1, lst2):
+  for item in lst2:
+    lst1.append(item)
+  lst1.sort()
+  return lst1
