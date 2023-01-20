aae3144cc1c9870fa90e887a91b7ce944ed5377a
David <dryan10101@gmail.com>
2023-01-20 01:39:54 -0800

    second set of py coding challenges

--- learned_pys/basic_challenge_qs.py
+++ learned_pys/basic_challenge_qs.py
@@ -23,3 +23,36 @@ def not_sum_to_ten(num1, num2):
   if num1 + num2 == 10:
     return False
   return True
+
+def in_range(num, lower, upper):
+  if (num >= lower) and (num <= upper):
+    return True
+  return False
+
+def same_name(your_name, my_name):
+  if your_name == my_name:
+    return True
+  return False
+
+def always_false(num):
+  if num > 2 and num < 2:
+    return True
+  return False
+
+def movie_review(rating):
+  if rating <= 5:
+    return "Avoid at all costs!"
+  elif rating > 5 and rating < 9:
+    return "This one was fun."
+  elif rating >= 9:
+    return "Outstanding!"
+
+def max_num(num1, num2, num3):
+  biggest = num1
+  if num2 >= biggest:
+    biggest = num2
+  if num3 >= biggest:
+    biggest = num3
+  if (biggest == num1 and biggest == num2) or (biggest == num1 and biggest == num3) or (biggest == num2 and biggest == num3):
+    return "It's a tie!"
+  return biggest
