049145b634f49e7695175beec8a8ee917e22454c
David <dryan10101@gmail.com>
2023-01-21 18:19:03 -0800

    updated the string methods lesson

--- learned_pys/strings_lesson.py
+++ learned_pys/strings_lesson.py
@@ -41,3 +41,24 @@ def username_generator(first_name, last_name):
 
 def password_generator(user_name):
   return user_name[-1] + user_name[:-1]
+
+#string methods review
+highlighted_poems = "Afterimages:Audre Lorde:1997,  The Shadow:William Carlos Williams:1915, Ecstasy:Gabriela Mistral:1925,   Georgia Dusk:Jean Toomer:1923,   Parting Before Daybreak:An Qi:2014, The Untold Want:Walt Whitman:1871, Mr. Grumpledump's Song:Shel Silverstein:2004, Angel Sound Mexico City:Carmen Boullosa:2013, In Love:Kamala Suraiyya:1965, Dream Variations:Langston Hughes:1994, Dreamwood:Adrienne Rich:1987"
+
+highlighted_poems_list = highlighted_poems.split(",")
+highlighted_poems_stripped = []
+for poem in highlighted_poems_list:
+  highlighted_poems_stripped.append(poem.strip())
+highlighted_poems_details = []
+for poem in highlighted_poems_stripped:
+  highlighted_poems_details.append(poem.split(":"))
+titles = []
+poets = []
+dates = []
+for stuff in highlighted_poems_details:
+  titles.append(stuff[0])
+  poets.append(stuff[1])
+  dates.append(stuff[2])
+
+for i in range(len(titles)):
+  print("The poem {} was published by {} in {}.".format(titles[i], poets[i], dates[i]))
