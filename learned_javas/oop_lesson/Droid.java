31b07bbf3760e2a015c775a9f76471339e7f4393
David <dryan10101@gmail.com>
2023-01-24 15:10:52 -0800

    added second oop lesson project

---
+++ learned_javas/oop_lesson/Droid.java
@@ -0,0 +1,25 @@
+public class Droid {
+  String name;
+  int batteryLevel;
+
+  public Droid(String droidName) {
+    name = droidName;
+    batteryLevel = 100;
+  }
+
+  public String toString() {
+    return "Hello, I'm the droid: " + name;
+  }
+
+  public void performTask(String task) {
+    System.out.println(name + " is performing task: " + task);
+    batteryLevel -= 10;
+  }
+
+  public static void main(String[] args) {
+    Droid codey = new Droid("Codey");
+    System.out.println(codey);
+    codey.performTask("drive to sea world");
+    codey.performTask("drive home from sea world");
+  }
+}
