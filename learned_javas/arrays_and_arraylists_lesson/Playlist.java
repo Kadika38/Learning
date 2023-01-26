4d7c3945906b9d41321c6519a3bc54250b803870
David <dryan10101@gmail.com>
2023-01-26 02:11:15 -0800

    added arrays and arraylists lesson folder and project within it

---
+++ learned_javas/arrays_and_arraylists_lesson/Playlist.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+
+class Playlist {
+  
+  public static void main(String[] args) {
+    ArrayList<String> desertIslandPlaylist = new ArrayList<String>();
+    desertIslandPlaylist.add("OMG");
+    desertIslandPlaylist.add("Kickflip");
+    desertIslandPlaylist.add("roll the windows up");
+    desertIslandPlaylist.add("608");
+    desertIslandPlaylist.add("Gucci Knife");
+    desertIslandPlaylist.add("Sorry Mama");
+    System.out.println(desertIslandPlaylist);
+    System.out.println(desertIslandPlaylist.size());
+    desertIslandPlaylist.remove("608");
+    //switch placement of two songs
+    int rtwu = desertIslandPlaylist.get("roll the windows up");
+    int omg = desertIslandPlaylist.get("OMG");
+    String a = "OMG";
+    desertIslandPlaylist.set(omg, desertIslandPlaylist.get(rtwu));
+    desertIslandPlaylist.set(rtwu, a);
+    System.out.println(desertIslandPlaylist);
+  }
+  
+}
