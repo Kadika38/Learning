16d8851688a4bf6a4d41290c41b6c33d5e06b715
David <dryan10101@gmail.com>
2023-01-31 17:06:54 -0800

    updated linked lists lesson for an incorrect copy paste issue - correct file was pasted in from the other editor now

--- pti_java/linked_lists_lesson/LinkedList.java
+++ pti_java/linked_lists_lesson/LinkedList.java
@@ -1,33 +1,63 @@
-public class Node {
+public class LinkedList {
 
-  public String data;
-  private Node next;	
-
-  public Node(String data) {
-    this.data = data;
-    this.next = null;
-  }
-
-  public void setNextNode(Node node) {
-    this.next = node;
-  }
-
-  public Node getNextNode() {
-    return this.next;
-  }
-
-  public static void main(String[] args) {
-    Node strawberry = new Node("Berry Tasty");
-    Node banana = new Node("Banana-rama");
-    Node coconut = new Node("Nuts for Coconut");
-    strawberry.setNextNode(banana);
-    banana.setNextNode(coconut);
-    Node currentNode = strawberry;
-    while (currentNode != null) {
-      System.out.println(currentNode.data);
-      currentNode = currentNode.getNextNode();
+    public static void main(String []args) {
+      // Write your code here:    
+      LinkedList seasons = new LinkedList();
+      seasons.addToHead("summer");
+      seasons.addToHead("spring");
+      seasons.addToTail("fall");
+      seasons.addToTail("winter");
+      seasons.removeHead();
+      seasons.printList();
+  
     }
-
+  
+      public Node head;
+  
+      public LinkedList() {
+          this.head = null;
+      }
+  
+    public void addToHead(String data) {
+      Node newHead = new Node(data);
+      Node currentHead = this.head;
+      this.head = newHead;
+      if (currentHead != null) {
+        this.head.setNextNode(currentHead);
+      }
+    }
+  
+    public void addToTail(String data) {
+      Node tail = this.head;
+      if (tail == null) {
+        this.head = new Node(data);
+      } else {
+        while (tail.getNextNode() != null) {
+          tail = tail.getNextNode();
+        }
+          tail.setNextNode(new Node(data));
+      }
+    }  
+  
+    public String removeHead() {
+      Node removedHead = this.head;
+      if (removedHead == null) {
+        return null;
+      }
+      this.head = removedHead.getNextNode();
+      return removedHead.data;
+    }
+  
+    public String printList() {
+      String output = "<head> ";
+      Node currentNode = this.head;
+      while (currentNode != null) {
+        output += currentNode.data + " ";
+        currentNode = currentNode.getNextNode();
+      }
+      output += "<tail>";
+      System.out.println(output);
+      return output;
+    }  
+  
   }
-
-}
