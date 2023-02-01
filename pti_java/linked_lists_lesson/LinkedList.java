19d36c1b62dbcc795e157135c27afe1295caf083
David <dryan10101@gmail.com>
2023-01-31 17:42:28 -0800

    added challenge method: swap, within LinkedList lesson

--- pti_java/linked_lists_lesson/LinkedList.java
+++ pti_java/linked_lists_lesson/LinkedList.java
@@ -9,6 +9,10 @@ public class LinkedList {
       seasons.addToTail("winter");
       seasons.removeHead();
       seasons.printList();
+
+      //test swap method
+      seasons.swap("summer", "fall");
+      seasons.printList();
   
     }
   
@@ -58,6 +62,74 @@ public class LinkedList {
       output += "<tail>";
       System.out.println(output);
       return output;
-    }  
+    }
+
+    //challenge method - swaps two nodes within the list
+    public void swap(String data1, String data2) {
+        Node n1 = null;
+        Node n1Prev = null;
+        boolean n1f = false;
+        Node n2 = null;
+        Node n2Prev = null;
+        boolean n2f = false;
+        //check if either node is the head
+        if (this.head.data == data1) {
+            n1 = this.head;
+            n1Prev = null;
+            n1f = true;
+        }
+        if (this.head.data == data2) {
+            n2 = this.head;
+            n2Prev = null;
+            n2f = true;
+        }
+        if (!n1f) {
+            n1Prev = this.head;
+            n1 = this.head.getNextNode();
+            while (!n1f) {
+                if (n1.data == data1) {
+                    n1f = true;
+                } else {
+                    n1Prev = n1;
+                    n1 = n1.getNextNode();
+                    if (n1 == null) {
+                        break;
+                    }
+                }
+            }
+        }
+        if (!n2f) {
+            n2Prev = this.head;
+            n2 = this.head.getNextNode();
+            while (!n2f) {
+                if (n2.data == data2) {
+                    n2f = true;
+                } else {
+                    n2Prev = n2;
+                    n2 = n2.getNextNode();
+                    if (n2 == null) {
+                        break;
+                    }
+                }
+            }
+        }
+        if (n1f && n2f) {
+            //switch previous node pointers
+            if (n1Prev != null) {
+                n1Prev.setNextNode(n2);
+            } else {
+                this.head = n2;
+            }
+            if (n2Prev != null) {
+                n2Prev.setNextNode(n1);
+            } else {
+                this.head = n1;
+            }
+            //switch next node pointers
+            Node temp1 = n1.getNextNode();
+            n1.setNextNode(n2.getNextNode());
+            n2.setNextNode(temp1);
+        }
+    }
   
   }
