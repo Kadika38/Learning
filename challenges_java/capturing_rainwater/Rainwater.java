a5f5e1bf3527640a223a7d1a60ae6da29ada217e
David <dryan10101@gmail.com>
2023-02-09 00:59:22 -0800

    finished rainwater challenge

--- challenges_java/capturing_rainwater/Rainwater.java
+++ challenges_java/capturing_rainwater/Rainwater.java
@@ -6,29 +6,52 @@ public class Rainwater {
     
     public int rainCaptured(int[] arr) {
         int total = 0;
+        boolean lookingForTop = true;
         if (arr.length > 0) {
+            //iterate through once and find high points in list
             ArrayList<Integer> highpoints = new ArrayList<Integer>();
-            int firstBiggest = arr[0];
-            int firstBiggestIndex = 0;
-            boolean fbFound = false;
-            for (int i = 1; i < arr.length; i++) {
-                if (arr[i] > firstBiggest && !fbFound) {
-                    firstBiggest = arr[i];
-                    firstBiggestIndex = i;
-                    if (arr[i+1] < arr[i]) {
-                        fbFound = true;
-                    }
-                }
-                if (fbFound && highpoints.size() == 0) {
-                    highpoints.add(firstBiggestIndex);
-                } else if (fbFound && highpoints.size() > 0) {
-                    if () {
-
-                    }
+            for (int i = 0; i < arr.length; i++) {
+                if (lookingForTop && i != arr.length - 1 && arr[i] >= arr[i+1]) {
+                    highpoints.add(i);
+                    lookingForTop = false;
+                } else if (!lookingForTop && i != arr.length - 1 && arr[i] < arr[i+1]) {
+                    lookingForTop = true;
+                } else if (lookingForTop && i == arr.length - 1) {
+                    highpoints.add(i);
                 }
             }
-        }
 
+            //iterate through again and calculate water caught using high points list
+            int current = 0;
+            while (current < highpoints.size() - 1) {
+                //left sides trough
+                if (arr[highpoints.get(current)] > arr[highpoints.get(current+1)]) {
+                    int waterLine = highpoints.get(current);
+                    while (arr[waterLine] > arr[highpoints.get(current+1)]) {
+                        waterLine++;
+                    }
+                    for (int i = waterLine; i < highpoints.get(current+1); i++) {
+                        total += (arr[highpoints.get(current+1)] - arr[i]);
+                    }
+                //right sided trough
+                } else if (arr[highpoints.get(current)] < arr[highpoints.get(current+1)]) {
+                    int i = highpoints.get(current) + 1;
+                    while (arr[i] < arr[highpoints.get(current)]) {
+                        total += (arr[highpoints.get(current) - arr[i]]);
+                        i++;
+                    }
+                //equal sided trough
+                } else if (arr[highpoints.get(current)] == arr[highpoints.get(current+1)]) {
+                    int waterLine = arr[highpoints.get(current)];
+                    for (int i = highpoints.get(current) + 1; i < highpoints.get(current+1); i++) {
+                        total += waterLine - arr[i];
+                    }
+                }
+                current++;
+            }
+
+            //System.out.println(highpoints);
+        }
         return total;
     }
 
